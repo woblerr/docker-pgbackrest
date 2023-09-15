1f2ef4ac140396db77ca83b4f2e57949973ef3a9
woblerr <antkurochkin@gmail.com>
2023-09-16 00:15:44 +0300

    Add test for sftp for tls communication protocol.
    An sftp repository has been added to the test logic for tls connection.

--- e2e_tests/conf/pg/pg_prepare-tls.sh
+++ e2e_tests/conf/pg/pg_prepare-tls.sh
@@ -8,5 +8,9 @@ PG_CLUSTER="main"
 PG_BIN="/usr/lib/postgresql/${PG_VERSION}/bin"
 PG_DATA="/var/lib/postgresql/${PG_VERSION}/${PG_CLUSTER}"
 
+# Add host to known_hosts.
+# Necessary for pgBackRest to work correctly over sftp.
+ssh-keyscan -t rsa -p 2222 sftp-tls > ~/.ssh/known_hosts
+
 # Start postgres.
 pg_ctlcluster ${PG_VERSION} ${PG_CLUSTER} start --foreground
